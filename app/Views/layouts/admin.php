840947fe5213b6794420650063e94b52ec330ea2
helios-byte <bambang2638@gmail.com>
2026-03-06 13:16:09 +0700

    administrator

--- app/Views/layouts/admin.php
+++ app/Views/layouts/admin.php
@@ -427,7 +427,7 @@
                     </div>
                     <div class="flex-1 min-w-0">
                         <div class="font-semibold text-gray-800 truncate">Admin</div>
-                        <div class="text-xs text-gray-600">Dimas Syahrul S (Owner)</div>
+                        <div class="text-xs text-gray-600">Administrator</div>
                     </div>
                 </div>
             </div>
