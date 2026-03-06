aaef19a624e146748907e5d06470993465037c78
DimasSyahrul17 <dimassyahrul1710@gmail.com>
2026-03-06 10:34:13 +0700

    menghapus semua test

--- check_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-
-$db = \Config\Database::connect();
-$fields = $db->getFieldNames('bookings');
-
-echo "Kolom di tabel bookings:\n";
-print_r($fields);
--- check_schema.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$_SERVER['REQUEST_METHOD'] = 'GET';
-$_SERVER['REQUEST_URI'] = '/';
-
-require_once __DIR__ . '/public/index.php';
